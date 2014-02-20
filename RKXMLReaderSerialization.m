edad189e5797a97e446c67961659c0d8aec41dac
John Haager <jhaager@topiatechnology.com>
2014-02-20 09:37:44 -0800

    Added in support for creating an XML string from an object.  It only creates elements, not attributes

--- RKXMLReaderSerialization.m
+++ RKXMLReaderSerialization.m
@@ -27,9 +27,78 @@
     return [XMLReader dictionaryForXMLData:data error:error];
 }
 
+//	This method is implemented to create an XML document from and object.
+//  It accomplishes this by creating an XML element tag for every object in the
+//	object dictionary.  It does NOT support attributes at all.
 + (NSData *)dataFromObject:(id)object error:(NSError **)error
 {
-    return nil;
+    NSDictionary *dataDict = (NSDictionary *)object ;
+    NSString *rootElement = [[ object keyEnumerator ] nextObject ] ;
+    
+    NSLog ( @"[ RKXMLReaderSerialization dataFromObject:error: -- %@", object ) ;
+    NSString *xmlString = [ NSString stringWithFormat:@"<%@>%@</%@>", rootElement, [ self contentOfDict:[ dataDict objectForKey:rootElement ] ], rootElement ] ;
+    return [ xmlString dataUsingEncoding:NSUTF8StringEncoding ] ;
+}
+
++ (NSString *)contentOfDict:(NSDictionary *)dict
+{
+    NSMutableString *xmlString = [ NSMutableString string ]  ;
+    
+    NSEnumerator *keyEnum = [ dict keyEnumerator ] ;
+    
+    for ( NSString * subKey in keyEnum )
+    {
+        if ( [ subKey isEqualToString:@"text" ] )
+        {
+            NSObject *value = [ dict objectForKey:subKey ] ;
+            if ( [ value isKindOfClass:[ NSString class ] ] )
+            {
+                [ xmlString  appendString:(NSString *)value  ] ;
+            }
+            if ( [ value isKindOfClass:[ NSNumber class ] ] )
+            {
+                [ xmlString appendString:[ (NSNumber *)value stringValue ] ] ;
+            }
+        }
+        else
+        {
+            NSObject *subObj = [ dict objectForKey:subKey ] ;
+            if ( [ subObj isKindOfClass:[ NSDictionary class ] ] )
+            {
+                NSDictionary *subDict = (NSDictionary *)subObj ;
+                if ( [ subDict count ] > 0 )
+                    [ xmlString  appendString:[ NSString stringWithFormat:@"<%@>%@</%@>", subKey, [ self contentOfDict:subDict ], subKey ] ] ;
+            }
+            if ( [ subObj isKindOfClass:[ NSArray class ] ] )
+            {
+                NSArray *subArray = (NSArray *)subObj ;
+                [ xmlString appendString:[ NSString stringWithFormat:@"%@", [ self contentOfArray:subArray withKey:subKey] ] ];
+            }
+        }
+    }
+    
+    return xmlString ;
+}
+
++ (NSString *)contentOfArray:(NSArray *)array withKey:(NSString *)key
+{
+    NSMutableString *xmlString = [ NSMutableString string ]  ;
+    
+    for ( NSObject *obj in array )
+    {
+        if ( [ obj isKindOfClass:[ NSDictionary class ] ] )
+        {
+            NSDictionary *subDict = (NSDictionary *)obj ;
+            [ xmlString appendString:[ NSString stringWithFormat:@"<%@>%@</%@>", key, [ self contentOfDict:subDict ], key ] ] ;
+        }
+        if ( [obj isKindOfClass:[ NSArray class ] ] )
+        {
+            NSArray *subArray = (NSArray *)obj ;
+            [ xmlString appendString:[ NSString stringWithFormat:@"<%@>%@</%@>", key, [ self contentOfArray:subArray withKey:key ], key ] ];
+        }
+    }
+
+    return xmlString ;
 }
 
 @end
